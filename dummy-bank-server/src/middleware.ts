b89d2d7ed57d69762d93e9069d3a915961b7d943
samrat53 <samrat05hccs@gmail.com>
2025-02-04 02:33:23 +0530

    feat: verification api build

--- dummy-bank-server/src/middleware.ts
+++ dummy-bank-server/src/middleware.ts
@@ -1,24 +1,25 @@
-import express from "express";
+import { NextFunction, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 import { ExtendedRequest } from "./config/types";
 
-export default async function(req: ExtendedRequest, res: express.Response, next: express.NextFunction) {
+const verifyAccountNumber = async (req: ExtendedRequest, res: Response, next:NextFunction) => {
     const accountNum = Number(req.params.id);
     try {
         const id = await prisma.user.findFirst({
             where: {
                 accountNum: accountNum
             }
-        })
+        });
         if(id) {
             req.verify = true;
             next();
         }
-        else return res.status(403).json({message: "Your account number donot exists on this bank"});
+        else return res.status(401).json({ message: "Your account number couldnot be authenticated with the bank" });
     }
     catch(err) {
         console.log(err);
-        return res.status(500).json({message: "Bank Database busy"});
+        return res.status(500).json({ message: "Bank Database error" });
     }
 }
+export default verifyAccountNumber;
--- dummy-bank-server/src/wallet-api/index.ts
+++ dummy-bank-server/src/wallet-api/index.ts
@@ -1,8 +1,19 @@
-import { Router } from "express";
-
+import { NextFunction, Router, Response } from "express";
 const router = Router();
+import verifyAccountNumber from "../middleware"
+import { ExtendedRequest } from "../config/types";
 
-
+router.get("/verify-user/:id", (req: ExtendedRequest, res: Response, next:NextFunction) => {
+        verifyAccountNumber(req, res, next)
+    }, 
+    async(req: ExtendedRequest, res: Response) => {
+        if(req.verify) {
+            res.status(200).json({ message: 'verified' });
+            return;
+        } 
+        return ; // if authenticated, then the middleware will return, this line will not be reached
+    }
+);
 
 
 export default router;
